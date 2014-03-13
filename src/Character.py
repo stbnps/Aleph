a94cb88383e76b12f389879e6e8b37b4f1bd8312
DaGal <DaGal@Nubuk>
2014-03-13 17:18:42 +0100

    This shit wasn't working.

--- src/Character.py
+++ src/Character.py
@@ -7,66 +7,66 @@ import os
 
 
 def roundToInt(f):
-    # Is there some workaround for this stupid thing?
-    return int(round(f))
+	# Is there some workaround for this stupid thing?
+	return int(round(f))
 
 
 def isSolid(collisionMap, x, y):
-    x = roundToInt(x)
-    y = roundToInt(y)
+	x = roundToInt(x)
+	y = roundToInt(y)
 
-    clipRect = collisionMap.get_rect()
+	clipRect = collisionMap.get_rect()
 
-    if (x >= clipRect.w) or (x < 0) or (y >= clipRect.h) or (y < 0):
-        return True
+	if (x >= clipRect.w) or (x < 0) or (y >= clipRect.h) or (y < 0):
+		return True
 
-    return collisionMap.get_at((x, y)) == (255, 0, 0, 255)
+	return collisionMap.get_at((x, y)) == (255, 0, 0, 255)
 
 
 class Character(Entity):
 
-    def __init__(self, x, y, imageName=None, colorkey=None, coordsName=None, numImages=None, *args):
-        Entity.__init__(self, x, y, imageName, colorkey, coordsName, numImages)
-        self.speedX = 0
-        self.speedY = 0
-        self.controller = None
-        self.equippedWpn = None
-        self.attacking = False
-        self.atk_cooldown = 1.0  # Starts without cooldown
+	def __init__(self, x, y, imageName=None, colorkey=None, coordsName=None, numImages=None, *args):
+		Entity.__init__(self, x, y, imageName, colorkey, coordsName, numImages)
+		self.speedX = 0
+		self.speedY = 0
+		self.controller = None
+		self.equippedWpn = None
+		self.attacking = False
+		self.atk_cooldown = 1.0  # Starts without cooldown
 
-        if not imageName:
-            self.rect = pygame.Rect(x, y, 15, 25)
+		if not imageName:
+			self.rect = pygame.Rect(x, y, 15, 25)
 
-    def move(self, time, collisionMap):
-        shiftX = self.speedX * time
-        shiftY = self.speedY * time
-        rectX = self.rect.move(shiftX, 0)
-        rectY = self.rect.move(0, shiftY)
+	def move(self, time, collisionMap):
+		shiftX = self.speedX * time
+		shiftY = self.speedY * time
+		rectX = self.rect.move(shiftX, 0)
+		rectY = self.rect.move(0, shiftY)
 
-        # Not the best way of doing this
-        if self.is_colliding_x(collisionMap, rectX):
-            shiftX = 0
-        if self.is_colliding_y(collisionMap, rectY):
-            shiftY = 0
+		# Not the best way of doing this
+		if self.is_colliding_x(collisionMap, rectX):
+			shiftX = 0
+		if self.is_colliding_y(collisionMap, rectY):
+			shiftY = 0
 
-        self.rect.move_ip(shiftX, shiftY)
+		self.rect.move_ip(shiftX, shiftY)
 
-    def setWeapon(self, weapon):
-        self.equippedWpn = weapon
+	def setWeapon(self, weapon):
+		self.equippedWpn = weapon
 
-    def update(self, time, collisionMap):
-        self.move(time, collisionMap)
-        if self.equippedWpn:
-            self.equippedWpn.update(time, self)
+	def update(self, time, collisionMap):
+		self.move(time, collisionMap)
+		if self.equippedWpn:
+			self.equippedWpn.update(time, self)
 			self.update_attack_cooldown(time)
 
-    def draw(self, screen, camera):
-        Entity.draw(self, screen, camera)
+	def draw(self, screen, camera):
+		Entity.draw(self, screen, camera)
 
-        if self.equippedWpn:
-            self.equippedWpn.draw(screen, camera)
+		if self.equippedWpn:
+			self.equippedWpn.draw(screen, camera)
 
-    def update_attack_cooldown(self, time):
+	def update_attack_cooldown(self, time):
 		""" Reduces time remaining to next attack.
 		"""
 		if self.attacking:
@@ -74,54 +74,53 @@ class Character(Entity):
 		else:
 			self.atk_cooldown = 0
 
-    def is_atacking(self):
-        """ Returns true if player is atacking.
-        """
-        if self.can_attack() and self.attacking:
-            self.atk_cooldown = 10
-            return True
-        else:
-            return False
+	def is_attacking(self):
+		""" Returns true if player is atacking.
+		"""
+		if self.can_attack() and self.attacking:
+			return True
+		else:
+			return False
 
-    def can_attack(self):
-        """ Returns true when the character can attack
-        """
-        # De momento solo tenemos enfriamientos para no atacar chorrecientasmil veces
-        # con un solo click.
-        if self.atk_cooldown <= 0:
-        	self.atk_cooldown = 1.0
-        	return True
-        else:
-        	return False
+	def can_attack(self):
+		""" Returns true when the character can attack
+		"""
+		# De momento solo tenemos enfriamientos para no atacar chorrecientasmil veces
+		# con un solo click.
+		if self.atk_cooldown <= 0:
+			self.atk_cooldown = 1.0
+			return True
+		else:
+			return False
 
-    def has_melee_weapon(self):
-        """ Returns true if the player has equipped melee weapons.
-        """
-        # De momento siempre tenemos la espadita.
-        return True
+	def has_melee_weapon(self):
+		""" Returns true if the player has equipped melee weapons.
+		"""
+		# De momento siempre tenemos la espadita.
+		return True
 
-    def has_distance_weapon(self):
-        """ Returns true if the player has equipped distance weapons.
-        """
-        # De momento si es a distancia no puede ser cuerpo a cuerpo.
-        return not self.has_melee_weapon()
+	def has_distance_weapon(self):
+		""" Returns true if the player has equipped distance weapons.
+		"""
+		# De momento si es a distancia no puede ser cuerpo a cuerpo.
+		return not self.has_melee_weapon()
 
-    def is_colliding_x(self, collisionMap, rectX):
-        """ Returns true if collides in x axis.
-        """
-        return  isSolid(collisionMap, rectX.x, rectX.y) or\
-            isSolid(collisionMap, rectX.x, rectX.bottom) or\
-            isSolid(collisionMap, rectX.x, rectX.centery) or\
-            isSolid(collisionMap, rectX.right, rectX.y) or\
-            isSolid(collisionMap, rectX.right, rectX.bottom) or\
-            isSolid(collisionMap, rectX.right, rectX.centery)
+	def is_colliding_x(self, collisionMap, rectX):
+		""" Returns true if collides in x axis.
+		"""
+		return  isSolid(collisionMap, rectX.x, rectX.y) or\
+			isSolid(collisionMap, rectX.x, rectX.bottom) or\
+			isSolid(collisionMap, rectX.x, rectX.centery) or\
+			isSolid(collisionMap, rectX.right, rectX.y) or\
+			isSolid(collisionMap, rectX.right, rectX.bottom) or\
+			isSolid(collisionMap, rectX.right, rectX.centery)
 
-    def is_colliding_y(self, collisionMap, rectY):
-        """ Returns true if collides in y axis.
-        """
-        return isSolid(collisionMap, rectY.x, rectY.y) or\
-            isSolid(collisionMap, rectY.x, rectY.bottom) or\
-            isSolid(collisionMap, rectY.centerx, rectY.y) or\
-            isSolid(collisionMap, rectY.right, rectY.y) or\
-            isSolid(collisionMap, rectY.right, rectY.bottom) or\
-            isSolid(collisionMap, rectY.centerx, rectY.bottom)
+	def is_colliding_y(self, collisionMap, rectY):
+		""" Returns true if collides in y axis.
+		"""
+		return isSolid(collisionMap, rectY.x, rectY.y) or\
+			isSolid(collisionMap, rectY.x, rectY.bottom) or\
+			isSolid(collisionMap, rectY.centerx, rectY.y) or\
+			isSolid(collisionMap, rectY.right, rectY.y) or\
+			isSolid(collisionMap, rectY.right, rectY.bottom) or\
+			isSolid(collisionMap, rectY.centerx, rectY.bottom)
