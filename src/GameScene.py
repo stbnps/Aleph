15d5ee75458c85db1ff7f3e407b0799510e564c5
DaGal <DaGal@HELIOS>
2014-03-15 15:12:01 +0100

    Added enemy sprites: Newton, Mr. H, nazists and wolves. I think it's enough for levels 2 and 3.

--- src/GameScene.py
+++ src/GameScene.py
@@ -24,8 +24,13 @@ class GameScene(Scene):
 	def __init__(self, director, player):
 		Scene.__init__(self, director)
 		self.player = player
-		#self.enemy = Enemy(300, 250, "player-alt.png", -1, "coordPlayerAlt2.txt", [3, 3, 3, 3], player)
-		self.enemyGroup = EntityGroup([Enemy(300, 250, "player-alt.png", -1, "coordPlayerAlt2.txt", [3, 3, 3, 3], player)])
+		# self.enemy = Enemy(300, 250, "player-alt.png", -1, "coordPlayerAlt2.txt", [3, 3, 3, 3], player)
+		self.enemyGroup = EntityGroup([])
+		# self.enemyGroup.add([Enemy(300, 280, "mr_h.png", -1, "coordMr_h.txt", [3, 3, 3, 3], player)])
+		# Commenting enemies because I don't want them to colapse on into another. I'll make collisions l8r.
+		# self.enemyGroup.add([Enemy(300, 280, "nazist.png", -1, "coordNazist.txt", [3, 3, 3, 3], player)])
+		# self.enemyGroup.add([Enemy(300, 280, "newton.png", -1, "coordNewton.txt", [3, 3, 3, 3], player)])
+		self.enemyGroup.add([Enemy(300, 280, "wolves.png", -1, "coordWolves.txt", [3, 3, 3, 3], player)])
 		self.bg = load_image("map_newton_img.png", Constants.MAP_DIR)
 		self.collisionBg = load_image("map_newton_bg.png", Constants.BG_MAP_DIR)
 		# self.collisioBg = self.bg.copy()
@@ -36,7 +41,7 @@ class GameScene(Scene):
 		self.bullet = None
 		# self.player.setWeapon(WpnBlade(player.rect.x, player.rect.y, "wpns2.png", -1, pygame.Rect(343, 341, 30, 30)))
 		self.player.setWeapon(WpnBlade(player.rect.x, player.rect.y, "lightsaber.png", -1, pygame.Rect(128, 77, 42, 42)))
-		
+
 		self.HUD = HUD.HUD((0, 467), True)
 		hudLayer = Layer(self.director)
 		hudLayer.append(self.HUD)
@@ -45,7 +50,7 @@ class GameScene(Scene):
 	def update(self, time):
 		self.player.controller.update(time, self.collisionBg)
 		self.enemyGroup.update(time, self.collisionBg)
-		#self.enemy.controller.update(time, self.collisionBg)
+		# self.enemy.controller.update(time, self.collisionBg)
 		# self.player.update(time, self.collisionBg)
 
 		if self.bullet != None:
@@ -80,7 +85,7 @@ class GameScene(Scene):
 			self.bullet.draw(screen, self.camera)
 
 		self.player.draw(screen, self.camera)
-		#self.enemy.draw(screen, self.camera)
+		# self.enemy.draw(screen, self.camera)
 		self.enemyGroup.draw(screen, self.camera)
 		# TODO: move maps and characters to its own layer
-		Scene.draw(self, screen) # draws rest of layers
+		Scene.draw(self, screen)  # draws rest of layers
